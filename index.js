ab66c4af461403e9d33f20f7ebde4095928c053f
Mehak Mahajan <mehekkdps17@gmail.com>
2024-06-14 00:52:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,22 +13,43 @@ function makesound(key){
         case "w":
             var audio=new Audio('sounds/tom-1.mp3').play();
             break;
+        case "W":
+            var audio=new Audio('sounds/tom-1.mp3').play();
+            break;
         case "a":
+            var audio=new Audio('sounds/tom-2.mp3').play();
+            break;
+        case "A":
             var audio=new Audio('sounds/tom-2.mp3').play();
             break;
         case "s":
             var audio=new Audio('sounds/tom-3.mp3').play();
             break;
+        case "S":
+            var audio=new Audio('sounds/tom-3.mp3').play();
+            break;
         case "d":
+            var audio=new Audio('sounds/tom-4.mp3').play();
+            break;
+        case "D":
             var audio=new Audio('sounds/tom-4.mp3').play();
             break;
         case "j":
             var audio=new Audio('sounds/snare.mp3').play();
             break;
+        case "J":
+            var audio=new Audio('sounds/snare.mp3').play();
+            break;
         case "k":
             var audio=new Audio('sounds/crash.mp3').play();
             break;
+        case "K":
+            var audio=new Audio('sounds/crash.mp3').play();
+            break;
         case "l":
+            var audio=new Audio('sounds/kick-bass.mp3').play();
+            break;
+        case "L":
             var audio=new Audio('sounds/kick-bass.mp3').play();
             break;
       }
